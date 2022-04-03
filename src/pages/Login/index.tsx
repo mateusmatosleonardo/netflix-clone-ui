dcab631f05023a25584126088a55292f2ad37c0a
Mateus Leonardo <m4teusmatos@gmail.com>
2022-04-03 18:24:08 -0300

    finalizando login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,22 +1,72 @@
 import React from 'react';
-import {Image, StyleSheet, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {Image, StyleSheet, Text, TextInput, View} from 'react-native';
 import {theme} from '../../styles/theme';
-import {Container, Header, Touched} from './styles';
+import {
+  BtnSignIn,
+  Container,
+  ContainerInput,
+  Header,
+  InputArea,
+  RetrieverPasswordText,
+  Touched,
+} from './styles';
 import logoNT from '../../assets/icons/logoNt.png';
 import arrowLeft from '../../assets/icons/arrow-left.png';
 
 const Login = () => {
+  const navigation = useNavigation();
+
   return (
     <Container style={styles.container}>
       <Header>
-        <Touched activeOpacity={0.7} style={styles.touched}>
+        <Touched
+          activeOpacity={0.7}
+          style={styles.touched}
+          onPress={() => navigation.goBack()}>
           <Image source={arrowLeft} style={styles.arrow} />
         </Touched>
         <Image source={logoNT} style={{width: 105.6, height: 28.57}} />
-        <Touched>
+        <Touched activeOpacity={0.7}>
           <Text style={styles.textHelp}>Help</Text>
         </Touched>
       </Header>
+      <ContainerInput>
+        <InputArea style={styles.inputArea}>
+          <TextInput
+            placeholderTextColor={theme.colors.white}
+            placeholder="E-mail"
+            style={{
+              fontSize: 14,
+              paddingLeft: 8,
+              fontFamily: theme.fonts.manrope,
+            }}
+          />
+        </InputArea>
+        <InputArea style={[styles.inputArea, {marginTop: 20}]}>
+          <TextInput
+            placeholderTextColor={theme.colors.white}
+            placeholder="Senha"
+            style={{
+              fontSize: 14,
+              paddingLeft: 8,
+              fontFamily: theme.fonts.manrope,
+            }}
+          />
+        </InputArea>
+      </ContainerInput>
+      <BtnSignIn
+        activeOpacity={0.7}
+        onPress={() => navigation.navigate('Tabs')}>
+        <Text style={styles.titleBtn}>Entrar</Text>
+      </BtnSignIn>
+      <View style={{width: '100%', alignItems: 'center', marginTop: 12}}>
+        <Touched activeOpacity={0.7}>
+          <RetrieverPasswordText style={styles.retrieverText}>
+            Retriever password
+          </RetrieverPasswordText>
+        </Touched>
+      </View>
     </Container>
   );
 };
@@ -40,6 +90,20 @@ const styles = StyleSheet.create({
   textHelp: {
     color: theme.colors.white,
     fontFamily: theme.fonts.manrope,
+  },
+  inputArea: {
+    height: 44,
+    backgroundColor: 'rgba(255, 255, 255, 0.2)',
+    borderRadius: 5,
+    borderWidth: 0,
+  },
+  titleBtn: {
+    fontFamily: theme.fonts.manropeMedium,
+    color: theme.colors.white,
+  },
+  retrieverText: {
+    fontFamily: theme.fonts.manropeMedium,
+    color: theme.colors.white,
   },
 });
 
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -13,3 +13,28 @@ export const Header = styled.View`
 `;
 
 export const Touched = styled.TouchableOpacity``;
+
+export const ContainerInput = styled.View`
+  width: 100%;
+  margin-top: 200px;
+`;
+
+export const InputArea = styled.View`
+  width: 100%;
+  border-radius: 4px;
+  border: 1px solid #fafafa;
+`;
+
+export const BtnSignIn = styled.TouchableOpacity`
+  width: 100%;
+  height: 46px;
+  border-radius: 4px;
+  border: 1px solid #ee1520;
+  margin-top: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const RetrieverPasswordText = styled.Text`
+  font-size: 12px;
+`;
