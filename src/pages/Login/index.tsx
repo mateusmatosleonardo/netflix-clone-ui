70db87756f65c120f7af65812d884238c3fca50c
Mateus Leonardo <m4teusmatos@gmail.com>
2022-04-03 13:13:10 -0300

    criando header do login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
-import {StyleSheet, Text} from 'react-native';
+import {Image, StyleSheet, Text} from 'react-native';
 import {theme} from '../../styles/theme';
-import {Container} from './styles';
+import {Container, Header, Touched} from './styles';
+import logoNT from '../../assets/icons/logoNt.png';
+import arrowLeft from '../../assets/icons/arrow-left.png';
 
 const Login = () => {
   return (
     <Container style={styles.container}>
-      <Text>Login</Text>
+      <Header>
+        <Touched activeOpacity={0.7} style={styles.touched}>
+          <Image source={arrowLeft} style={styles.arrow} />
+        </Touched>
+        <Image source={logoNT} style={{width: 105.6, height: 28.57}} />
+        <Touched>
+          <Text style={styles.textHelp}>Help</Text>
+        </Touched>
+      </Header>
     </Container>
   );
 };
@@ -16,6 +26,20 @@ const styles = StyleSheet.create({
     paddingHorizontal: 30,
     paddingTop: 10,
     backgroundColor: theme.colors.blackLight,
+  },
+  arrow: {
+    width: 22,
+    height: 22,
+  },
+  touched: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    paddingHorizontal: 9,
+    paddingVertical: 9,
+    borderRadius: 12,
+  },
+  textHelp: {
+    color: theme.colors.white,
+    fontFamily: theme.fonts.manrope,
   },
 });
 
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -3,3 +3,13 @@ import styled from 'styled-components/native';
 export const Container = styled.ScrollView`
   flex: 1;
 `;
+
+export const Header = styled.View`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 5px;
+`;
+
+export const Touched = styled.TouchableOpacity``;
